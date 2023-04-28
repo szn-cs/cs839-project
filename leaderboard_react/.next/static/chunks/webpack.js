6dc3b2b6fa6ee6baf4cc5abbb6c9b01c41802771
mmbayles <mmbayles@gmail.com>
2023-04-28 12:32:06 -0600

    Leaderboard can now connect to our DAO

--- leaderboard_react/.next/static/chunks/webpack.js
+++ leaderboard_react/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6581bcf10bed957"; }
+/******/ 		__webpack_require__.h = function() { return "39c809dbc76390f2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- leaderboard_react/src/pages/main.js
+++ leaderboard_react/src/pages/main.js
@@ -7,9 +7,41 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Web3 from 'web3';
 
 export default function MyApp() {
-    let leaders = [{"name":"Matthew","value":25},{"name":"Joe","value":55}, {"name":"Bob","value":1}]
+    const ROWLIMIT = 10
+
+    async function testBalance(){
+        const network = "https://api.avax-test.network/ext/bc/C/rpc"
+        const daoAdd = "0xEf84bE8dFc4B529F5e544d8B8D678f1b223F2B51"
+//        Gabriel
+        const testAdd = "0xF0B48DA5c26CA87AfD5b3d083A772E980a7001Fc"
+//        Matthew
+        const myAdd = "0x89BF19105d76033F328C54B8DDeA404A4b282Ef8"
+        const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"form","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
+
+        let web3 = new Web3(new Web3.providers.HttpProvider(network));
+        console.log(web3)
+        console.log(await web3.eth.net.getId())
+
+        // Replace <DAO_CONTRACT_ABI> and <DAO_CONTRACT_ADDRESS> with the ABI and address of your DAO contract
+        const daoContract = new web3.eth.Contract(abi, daoAdd);
+
+        // Replace <YOUR_WALLET_ADDRESS> with your wallet address
+        const balance = await daoContract.methods.balanceOf(testAdd).call();
+        const name = await daoContract.methods.name().call();
+        const methods = await daoContract.methods;
+
+        console.log('DAO balance:', balance);
+        console.log('DAO name:', name);
+        console.log('DAO methods:', methods);
+    }
+
+    testBalance()
+    let leaders = [{"name":"Matthew","message":"I'm the best", "value":25},
+        {"name":"Joe","message":"I'm the best","value":55},
+        {"name":"Bob","message":"I'm the best","value":1}]
     const compareByValue = (a, b) => {
       if (a.value > b.value) {
         return -1;
@@ -22,13 +54,14 @@ export default function MyApp() {
     leaders.sort(compareByValue);
     return (
         <div>
-          <Button variant="contained">Hello World</Button>
+
 
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
                     <TableCell>Name</TableCell>
+                    <TableCell >Message</TableCell>
                     <TableCell align="right">Total Tokens Bought</TableCell>
 
                   </TableRow>
@@ -42,6 +75,9 @@ export default function MyApp() {
                       <TableCell component="th" scope="row">
                         {row.name}
                       </TableCell>
+                      <TableCell component="th" scope="row">
+                        {row.message}
+                      </TableCell>
                       <TableCell align="right">{row.value}</TableCell>
 
                     </TableRow>
@@ -49,6 +85,7 @@ export default function MyApp() {
                 </TableBody>
             </Table>
         </TableContainer>
+        <Button variant="contained">Refresh</Button>
         </div>
     );
 }
